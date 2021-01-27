38dadf4c36d7b9d3d84446cdd6cc6711bd7fed6c
Daniel Acuña <hernandez.daniel1894@gmail.com>
2021-01-27 13:06:55 -0600

    diseño listo

--- Documents/Angular/nutricionPaoEG/src/app/app-routing.module.ts
+++ Documents/Angular/nutricionPaoEG/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { AboutComponent } from './pages/about/about.component';
 
-const routes: Routes = [];
+const app_routes: Routes = [
+  {path: 'home', component: PrincipalComponent},
+  {path: 'about', component:AboutComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(app_routes, {useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
