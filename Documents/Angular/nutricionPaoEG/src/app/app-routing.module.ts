29e1df14532c30ac5ab6de5930b99f9fed04484d
Daniel Acuña <hernandez.daniel1894@gmail.com>
2021-01-30 15:30:04 -0600

    commit

--- Documents/Angular/nutricionPaoEG/src/app/app-routing.module.ts
+++ Documents/Angular/nutricionPaoEG/src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PrincipalComponent } from './pages/principal/principal.component';
-import { AboutComponent } from './pages/about/about.component';
 
-const app_routes: Routes = [
-  {path: 'home', component: PrincipalComponent},
-  {path: 'about', component:AboutComponent},
-  {path: '**', pathMatch: 'full', redirectTo: 'home'}
-]
+const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(app_routes, {useHash:true})],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- Documents/Angular/nutricionPaoEG/src/app/app.module.ts
+++ Documents/Angular/nutricionPaoEG/src/app/app.module.ts
@@ -3,18 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { PrincipalComponent } from './pages/principal/principal.component';
-import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    PrincipalComponent,
-    AboutComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
